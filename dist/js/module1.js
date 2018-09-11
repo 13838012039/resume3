0cf486da31a47c73f6ab879a3f23f17b6fcdef8e
Bunkang.Lee <liwenhansj@163.com>
2018-09-12 00:32:57 +0800

    add dist

---
+++ dist/js/module1.js
@@ -0,0 +1,16 @@
+// var accessor = function() {
+//     var person = {
+//         son: 'lee',
+//         grilfriend: 'liu',
+//         age: 18
+//     }
+//     return function() {
+//         person.age += 1
+//         return person.age
+//     }
+
+// }
+
+
+// var growUp = accessor.call()
+// console.log(growUp.call())
--- js/module2.js
+++
@@ -1,5 +0,0 @@
-// ! function() {
-//     var frankGrowUp = a.frankGrowUp
-//     var newAge = frankGrowUp()
-//     console.log(newAge)
-// }.call()
